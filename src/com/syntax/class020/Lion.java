07449a6443484535c79cb87ae29abc3755a3d99b
RazielEL <amp91@pm.me>
2022-03-13 08:59:10 -0400

    commit

--- src/com/syntax/class020/Lion.java
+++ src/com/syntax/class020/Lion.java
@@ -2,11 +2,10 @@ package com.syntax.class020;
 
 public class Lion  extends Animal {
     boolean huntOthers = true;
+
     void roar(){
         System.out.println(name + " is roaring and is " + color + " and " + huntOthers);
     }
-
-
 
 
     public static void main(String[] args) {
