72f91f02e446f58844e227d170929de653802a4f
RazielEL <amp91@pm.me>
2022-04-26 14:28:31 -0500

    windows

--- src/com/syntax/Exercises/Main.java
+++ src/com/syntax/Exercises/Main.java
@@ -1,57 +1,26 @@
 package com.syntax.Exercises;
 
-import java.util.*;
-import java.util.Map.Entry;
-
+import java.util.Arrays;
 
 class Main {
-    public static void main (String[] args) {
-
-        List<Map<String, Object>> datalist = new ArrayList<>();
+    public static void main(String[] args) {
 
 
-        Map<String, Object> appleMap = new LinkedHashMap<>();
-
-        appleMap.put("Items", "Apple");
-        appleMap.put("Price", 20.00d);
-        appleMap.put("Quantity", 10);
-
-        datalist.add(appleMap);
-
-        Map<String, Object> orangeMap = new LinkedHashMap<>();
-
-        orangeMap.put("Items", "Orange");
-        orangeMap.put("Price", 21.99d);
-        orangeMap.put("Quantity", 10);
-
-        datalist.add(orangeMap);
-
-        double purchaseTotal = 0;
-
-        for (int i = 0; i < datalist.size(); i++) {
-
-            Map<String, Object> currentMapValue = datalist.get(i);
+        String s = "(())";
+        System.out.println(isBalanced(s));
 
 
-            String name = (String) currentMapValue.get("Items");
-            double price = (double) currentMapValue.get("Price");
-            int quantity = (int) currentMapValue.get("Quantity");
-            double quantity2 = quantity;
-            double subTotal = price * quantity;
-            purchaseTotal = purchaseTotal + subTotal;
+    }
 
 
-                System.out.println("Items: " + name + " Price: " + price + " Quantity: " + quantity2 + " SubTotal: " + subTotal);
-                System.out.println();
+    public static boolean isBalanced(String s) {
 
-            }
-        System.out.println("Your Purchase total:" + purchaseTotal);
-            }
+        boolean flag = false;
 
+        for (int i = 0; i < s.length(); i++) {
+            flag = s.charAt(i) == s.charAt(s.length() - 1 - i);
         }
 
-
-
-
-
-
+        return flag;
+    }
+}
